f77d10e19d7e1fba70797f6c77a754841c9eb17c
flowery-power <gabrovska.tsvetelina@gmail.com>
2023-03-23 21:04:29 +0200

    My first commit

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,17 @@
+package emailapp;
+
+public class EmailApp {
+
+    public static void main(String[]args){
+        Email em1= new Email("John","Smith");
+
+        System.out.print(em1.showInfo());
+
+//        em1.setAlternateEmail("js@gmail.com");
+//        System.out.print(em1.getAlternateEmail());
+
+
+
+    }
+
+}
